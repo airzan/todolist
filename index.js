f9683e298123c018aa9b9b375d3a833733b85450
Airton <airzan7@gmail.com>
2019-07-31 16:11:27 -0300

    get post & delete

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ $(".demo.index").ready(function(){
       success: function (response, textStatus) {
         $('#todo-list').empty();
         response.tasks.forEach(function (task) {
-          $('#todo-list').append('<p>' + task.content + '</p>');
-        })
+          $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button>');
+          });
       },
       error: function (request, textStatus, errorMessage) {
         console.log(errorMessage);
@@ -44,6 +44,22 @@ $(".demo.index").ready(function(){
     createTask();
   });
 
-  getAndDisplayAllTasks();
+  var deleteTask = function (id) {
+    $.ajax({
+      type: 'DELETE',
+      url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '?api_key=84',
+      success: function (response, textStatus) {
+        getAndDisplayAllTasks();
+      },
+      error: function (request, textStatus, errorMessage) {
+        console.log(errorMessage);
+      }
+    });
+  }
 
+  $(document).on('click', '.delete', function () {
+    deleteTask($(this).data('id'));
+  });
+
+  getAndDisplayAllTasks();
 });
